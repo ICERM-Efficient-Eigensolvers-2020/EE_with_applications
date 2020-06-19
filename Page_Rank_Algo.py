55665603846520be12f6e0a451854cb4a61c7315
yuqingl <yuqingl@umich.edu>
2020-06-19 12:04:57 -0400

    Page Rank Application Test

--- Page_Rank_Algo.py
+++ Page_Rank_Algo.py
@@ -1,6 +1,9 @@
 import Power_Iteration as pi
 import numpy as np
 import networkx as nx
+import scipy
+
+from matplotlib import pyplot as plt
 
 def power_iteration_application_test():
     A = np.array([[1.5, 0.5], [0.5, 1.5]])
@@ -11,32 +14,42 @@ def power_iteration_application_test():
 
 def page_rank_application_test():
     G = nx.DiGraph()
-    G.add_edges_from([(1,2),(2,3),(2,4), (3, 4),(3,5), (4, 5),(5,2),(5,4)])
-    print(PageRank(G, 0.85))
+    G.add_edges_from([(1,2),(1,3),(1,4),(2,3),(2,4), (3, 1),(4,1), (4, 3)])
+    nx.draw(G, with_labels=True)
+    plt.savefig("page_rank_application_sample.png")
+    plt.show()
+    print("Limiting Distribution: ")
+    print(PageRank(G, 0.15))
+
 
 def PageRank(G, weight):
 
-    Aj = nx.adjacency_matrix(G)
+    Aj = nx.to_numpy_matrix(G).A
     N = len(G.nodes)
 
     A = np.zeros(shape=(N,N))
-    M = np.zeros(shape=(N,N))
     S = np.ones(shape = (N,N))
+    S = np.multiply(S, 1/N)
+
     for j, node in enumerate(G.nodes()):
+        out_deg = G.out_degree(node)
         for i in range(N):
-            A[i][j] = Aj[i][j]/G.out_degree(node)
 
-    M =  A * (1-weight) -  S * weight
+            A[i][j] = Aj[i][j]/out_deg
 
+    Aw = np.multiply(A, 1 - weight)
+    Sw = np.multiply(S, weight)
+    M = Aw + Sw
 
-    print("under construction")
-    return pi.PowerMethod(M,True, 0.01)
+    return pi.PowerMethod(M,True, 0.01)[0]
 
 
 
 if __name__ == '__main__':
+    print("###Power Iteration Test###")
     power_iteration_application_test()
 
+    print("###Page Rank Test###")
     page_rank_application_test()
 
 
