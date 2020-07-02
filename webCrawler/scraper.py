c6e6d13f55c0926b541a678f85c5cf7a4d363568
Yuqing Liu <yuqingliu@Ericas-MacBook.local>
2020-07-02 11:24:45 -0400

    scraper

---
+++ webCrawler/scraper.py
@@ -0,0 +1,120 @@
+#copy right to: https://www.thepythoncode.com/article/extract-all-website-links-python
+import requests
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup
+import colorama
+import networkx as nx
+
+# init the colorama module
+colorama.init()
+GREEN = colorama.Fore.GREEN
+GRAY = colorama.Fore.LIGHTBLACK_EX
+RESET = colorama.Fore.RESET
+
+# initialize the set of links (unique links)
+internal_urls = set()
+external_urls = set()
+
+def is_valid(url):
+    """
+    Checks whether `url` is a valid URL.
+    """
+    parsed = urlparse(url)
+    return bool(parsed.netloc) and bool(parsed.scheme)
+
+def get_all_website_links(url):
+    """
+    Returns all URLs that is found on `url` in which it belongs to the same website
+    """
+    global diG
+    global url_dict
+    global idx
+    # all URLs of `url`
+    urls = set()
+    # domain name of the URL without the protocol
+    domain_name = urlparse(url).netloc
+    soup = BeautifulSoup(requests.get(url).content, "html.parser")
+
+    for a_tag in soup.findAll("a"):
+        href = a_tag.attrs.get("href")
+        if href == "" or href is None:
+            # href empty tag
+            continue
+
+        # join the URL if it's relative (not absolute link)
+        href = urljoin(url, href)
+        parsed_href = urlparse(href)
+        # remove URL GET parameters, URL fragments, etc.
+        href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
+        if not is_valid(href):
+            # not a valid URL
+            continue
+        if href in internal_urls:
+            # already in the set
+            continue
+        if domain_name not in href:
+            # external link
+            if href not in external_urls:
+                print(f"{GRAY}[!] External link: {href}{RESET}")
+                external_urls.add(href)
+            continue
+        print(f"{GREEN}[*] Internal link: {href}{RESET}")
+        urls.add(href)
+        internal_urls.add(href)
+
+        #add the internal link to url_dict
+        idx = idx + 1
+        diG.add_node(idx)
+        url_dict[href] = idx
+
+    return urls
+
+# number of urls visited so far will be stored here
+total_urls_visited = 0
+
+def crawl(url, max_urls=50):
+
+    """
+    Crawls a web page and extracts all links.
+    You'll find all links in `external_urls` and `internal_urls` global set variables.
+    params:
+        max_urls (int): number of max urls to crawl, default is 30.
+    """
+    global diG
+    global url_dict
+    global total_urls_visited
+    total_urls_visited += 1
+    links = get_all_website_links(url)
+    parent = url_dict[url]
+    for link in links:
+        child = url_dict[link]
+        diG.add_edge(parent, child)
+        if total_urls_visited > max_urls:
+            break
+        crawl(link, max_urls=max_urls)
+
+if __name__ == "__main__":
+    diG = nx.DiGraph()
+    url_dict = {}
+    idx = 0
+
+    url = "https://icerm.brown.edu"
+    max_urls = 30
+    url_dict[url] = idx
+    crawl(url, max_urls=max_urls)
+
+    print("[+] Total Internal links:", len(internal_urls))
+    print("[+] Total External links:", len(external_urls))
+    print("[+] Total URLs:", len(external_urls) + len(internal_urls))
+
+    domain_name = urlparse(url).netloc
+
+    # save the internal links to a file
+    with open(f"{domain_name}_internal_links.txt", "w") as f:
+        for internal_link in internal_urls:
+            print(internal_link.strip(), file=f)
+
+    # save the external links to a file
+    with open(f"{domain_name}_external_links.txt", "w") as f:
+        for external_link in external_urls:
+            print(external_link.strip(), file=f)
