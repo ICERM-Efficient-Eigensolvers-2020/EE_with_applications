d9668611a8d6cb0d7c11b032250ec5a33edb0d26
KellyRivera4 <58530072+KellyRivera4@users.noreply.github.com>
2020-07-16 09:19:15 -0400

    Update QR

--- efficientEigensolvers/QR_Algorithm.py
+++ efficientEigensolvers/QR_Algorithm.py
@@ -1,84 +1,113 @@
-## QR Algorithm
-# Author: Kelly Rivera
-
+"""
+author: "Kelly Rivera"
+title: "QR Algorithm"
+"""
 # This code is an implementation of a PageRank Algorithm variant using QR Decomposition
 # Implementing Householder and Gram-Schmidt Process
 
-### Import Libraries
+##Import Python Libraries
 import numpy as npy
-import scipy as sp
 import scipy.linalg as spl  # SciPy Linear Algebra Library
-import matplotlib.pyplot as plt
 
 
-### Householder transformation method
-def qr_householder(x):
-    Q, R = npy.linalg.qr(x)
-    return Q, R 
-
-def qr_Algorithm_HH(x, iterations):
+## Householder transformation method
+def qr_Algorithm_HH(x, converge_range):
     m, n = x.shape
     Q_last = npy.identity(n)
-    for _ in range(iterations):
-        Q, R = qr_householder(x)
+    diff = 1
+    
+    lamb, u = spl.eig(x)
+    lamb = min(npy.abs(lamb))
+    
+    # QR Algo using 
+    while diff > converge_range:
+        Q, R = npy.linalg.qr(x)
         # This update matrix A, by dot product of reverse QR 
         x = npy.dot(R, Q) 
+        
+        eigenvalue = npy.diag(x)  #outputs only the values on the main diaginal
+        eigenvalue = min(npy.abs(eigenvalue))
+        
+        diff = npy.abs(lamb - eigenvalue)
         
         eigenvectors = npy.dot(Q_last, Q)
         Q_last = eigenvectors
         
-    eigenvalues = npy.diag(x)  #outputs only the values on the main diaginal
-    return eigenvalues, eigenvectors
+    eigenvalues = npy.diag(x)  #outputs only the values on the main diaginal  
+    return eigenvalues, eigenvectors 
 
 
-### Gram-Schmidt Process
+## Gram-Schmidt Process
 def qr_GS(x):
     m, n = x.shape
     Q = npy.zeros((m, n)) # Q is all zero
     R = npy.zeros((n, n))
 
-    for j in range(n): 
+    for j in range(n): #range (3) = {0,1,2}
         v = x[:, j] # x[:,0] returns the first* column* of x, x[:,1] reutrns the second column etc
 
-        for i in range(j): 
-            q = Q[:, i]    
-            R[i, j] = q.dot(v)
-            v = v - R[i, j] * q
+        for i in range(j):
+            
+            Q1 = Q[:, i] #when we are here for the the first time, q = 
+            R[i, j] = npy.dot(Q1, v)
+            v = v - R[i, j] * Q1
 
         vNorm = npy.linalg.norm(v)
         R[j, j] = vNorm
         Q[:, j] = v / vNorm
-   
+    
     return Q, R
 
-def qr_Algorithm_GS(x, iterations):
+def qr_Algorithm_GS(x, converge_range):
     m, n = x.shape
     Q_last = npy.identity(n)
-    for _ in range(iterations):
+    diff = 1
+    
+    lamb, u = spl.eig(x)
+    lamb = min(npy.abs(lamb))
+        
+    while diff > converge_range:
         Q, R = qr_GS(x)
         # This update matrix A, by dot product of reverse QR 
         x = npy.dot(R, Q) 
         
+        eigenvalue = npy.diag(x)  #outputs only the values on the main diaginal
+        eigenvalue = min(npy.abs(eigenvalue))
+        
+        diff = npy.abs(lamb - eigenvalue)
+        
         eigenvectors = npy.dot(Q_last, Q)
         Q_last = eigenvectors
-    eigenvalues = npy.diag(x)  #outputs only the values on the main diaginal
-    return eigenvalues, eigenvectors
+        
+    eigenvalues = npy.diag(x)  #outputs only the values on the main diaginal    
+    
+    return eigenvalues, eigenvectors 
 
 
-### QR Gram-Schmidt with Shift 
-def shiftedQR_Algorithm(x, iterations):
+## QR with Shift using the Gram-Schmidt precess 
+def shiftedQR_Algorithm(x, converge_range):
     m, n = x.shape
     I = npy.identity(n)
     Q_last = npy.identity(n)
+    diff = 1
     
-    for _ in range(iterations):
+    lamb, u = spl.eig(x)
+    lamb = min(npy.abs(lamb))
+    
+    while diff > converge_range:
         μ = x[[n-1],[n-1]]         # shift: μ = a_nn
-        Q, R = qr_GS((x - (μ*I)))  # A − μI = QR: preforms the gram schmidt process with shift
-                                     
-        x = npy.dot(R,Q) + (μ*I)   # This update matrix A, by dot product of reverse QR and adding back the shift
+        Q, R = qr_GS((x - (μ*I)))
+        # This update matrix A, by dot product of reverse QR 
+        x = npy.dot(R, Q) + (μ*I)   # Updates matrix, by dot product of reverse QR and adding back the shift
+        
+        eigenvalue = npy.diag(x)  #outputs only the values on the main diaginal
+        eigenvalue = min(npy.abs(eigenvalue))
+        
+        diff = npy.abs(lamb - eigenvalue)
         
         eigenvectors = npy.dot(Q_last, Q)
         Q_last = eigenvectors
         
-    eigenvalues = npy.diag(x)  #outputs only the values on the main diaginal
-    return eigenvalues, eigenvectors 
+    eigenvalues = npy.diag(x)  #outputs only the values on the main diaginal    
+    
+    return eigenvalues, eigenvectors     
