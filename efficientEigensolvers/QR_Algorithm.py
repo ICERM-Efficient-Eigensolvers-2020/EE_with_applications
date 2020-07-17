2f8a6933643dde8a8c9c257a3e9cdf6e05a2f49f
KellyRivera4 <58530072+KellyRivera4@users.noreply.github.com>
2020-07-17 09:33:51 -0400

    Update QR_Algorithm.py

--- efficientEigensolvers/QR_Algorithm.py
+++ efficientEigensolvers/QR_Algorithm.py
@@ -2,9 +2,6 @@
 author: "Kelly Rivera"
 title: "QR Algorithm"
 """
-# This code is an implementation of a PageRank Algorithm variant using QR Decomposition
-# Implementing Householder and Gram-Schmidt Process
-
 ##Import Python Libraries
 import numpy as npy
 import scipy.linalg as spl  # SciPy Linear Algebra Library
@@ -33,7 +30,13 @@ def qr_Algorithm_HH(x, converge_range):
         eigenvectors = npy.dot(Q_last, Q)
         Q_last = eigenvectors
         
-    eigenvalues = npy.diag(x)  #outputs only the values on the main diaginal  
+        
+    eigenvalues = npy.diag(x)  #outputs only the values on the main diaginal
+    eigenvalues = max(npy.abs(eigenvalues))
+    eigenvectors = eigenvectors[[0][0]]
+    eigenvectors = eigenvectors[0]
+
+
     return eigenvectors, eigenvalues
 
 
@@ -65,7 +68,7 @@ def qr_Algorithm_GS(x, converge_range):
     
     lamb, u = spl.eig(x)
     lamb = min(npy.abs(lamb))
-        
+
     while diff > converge_range:
         Q, R = qr_GS(x)
         # This update matrix A, by dot product of reverse QR 
@@ -80,7 +83,10 @@ def qr_Algorithm_GS(x, converge_range):
         Q_last = eigenvectors
         
     eigenvalues = npy.diag(x)  #outputs only the values on the main diaginal    
-    
+    eigenvalues = max(npy.abs(eigenvalues))
+    eigenvectors = eigenvectors[[0][0]]
+    eigenvectors = eigenvectors[0]
+
     return eigenvectors, eigenvalues
 
 
@@ -90,7 +96,7 @@ def shiftedQR_Algorithm(x, converge_range):
     I = npy.identity(n)
     Q_last = npy.identity(n)
     diff = 1
-    
+   
     lamb, u = spl.eig(x)
     lamb = min(npy.abs(lamb))
     
@@ -109,5 +115,8 @@ def shiftedQR_Algorithm(x, converge_range):
         Q_last = eigenvectors
         
     eigenvalues = npy.diag(x)  #outputs only the values on the main diaginal    
-    
-    return eigenvectors, eigenvalues     
+    eigenvalues = max(npy.abs(eigenvalues))
+    eigenvectors = eigenvectors[[0][0]]
+    eigenvectors = eigenvectors[0]
+
+    return eigenvectors, eigenvalues    
